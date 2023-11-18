d509a386c06473e54849c874022023df3fa325ec
Tatiana B <tbeketova04@gmail.com>
2023-11-18 14:04:51 +0200

    create menu-burger

--- js/main.js
+++ js/main.js
@@ -1,6 +1,12 @@
 document.addEventListener('DOMContentLoaded', function() {
-  var elems = document.querySelectorAll('.sidenav');
-  var instances = M.Sidenav.init(elems);
+  var sidenav = document.querySelectorAll('.sidenav');
+  var instances_sidenav = M.Sidenav.init(sidenav);
+
+  var modal = document.querySelectorAll('.modal');
+  var instances_modal = M.Modal.init(modal);
+
+  var galerry = document.querySelectorAll('.materialboxed');
+    var instances_galerry = M.Materialbox.init(galerry);
 });
 
 // Initialize collapsible (uncomment the lines below if you use the dropdown variation)
@@ -12,3 +18,4 @@ document.addEventListener('DOMContentLoaded', function() {
 $(document).ready(function(){
   $('.sidenav').sidenav();
 });
+
